69bd0fcc05aa69f2fdcf186461189497cf0c6ea3
Aadirai132 <73470125+Aadirai132@users.noreply.github.com>
2020-11-17 14:19:43 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper {
+    constructor(x, y) {
+      var options = {
+        isStatic:false,
+          restitution:0.3,
+          friction:0.5,
+          density:0.5
+      }
+      this.body = Bodies.circle(x, y, 15, options);
+      //this.width = 50;
+      //this.height = 50;
+      //this.image = loadImage("paper.png"); 
+      World.add(world, this.body);
+    }
+    display()
+    {
+      var pos =this.body.position;
+      //var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      //rotate(angle);
+      ellipseMode(CENTER);
+      fill("purple");
+      stroke("purple");
+      strokeWeight(1);
+      ellipse(0, 0, 30, 30);
+      pop();
+    }
+  }
